c31392e45e8f9d273d978da90f4976c3a4f1447d
Rosario <Rosario@Rosario-MacBook-Pro.local>
2020-03-08 13:52:06 -0700

    Initial Commit

---
+++ Parstagram/FeedViewController.swift
@@ -0,0 +1,61 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Rosario on 3/3/20.
+//  Copyright © 2020 Rosario. All rights reserved.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        query.findObjectsInBackground{(posts,error) in
+            if posts != nil{
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return posts.count
+     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let  post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = post["caption"] as? String
+        
+        let ImageFile = post["image"] as! PFFileObject
+        let urlString = ImageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        return cell
+    }
+ 
+    
+}
